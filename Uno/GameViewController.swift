1128e54ea80f4d3fedcb47e0da7b39f047a37611
aacalfa <aacalfa@gmail.com>
2017-03-04 11:49:24 -0800

    first commit.

--- Uno/GameViewController.swift
+++ Uno/GameViewController.swift
@@ -14,7 +14,9 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+		// TODO: Testing
+		print("testing\na")
+		
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
         if let scene = GKScene(fileNamed: "GameScene") {
